4845606f5164c8edcaeb5a2090120e6d5e2573f3
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-11-13 23:38:47 +0200

    Winning/draw messages

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 const xClass = "x";
 const circleClass = "circle";
+const winningCombination = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [4, 4, 8],
+  [2, 4, 6],
+];
+
+const winningMessage = document.getElementById("winningMessage");
+const winningMessageText = document.querySelector(
+  "[data-winning-message-text]"
+);
 
 const cells = document.querySelectorAll("[data-cell]");
 const board = document.getElementById("board");
+const restartButton = document.getElementById("restart");
 
 let circleTurn;
 
 startGame();
 
+restartButton.addEventListener("click", startGame);
+
 function startGame() {
   circleTurn = false;
   cells.forEach((cell) => {
+    cell.classList.remove(xClass);
+    cell.classList.remove(circleClass);
+    cell.addEventListener("click", handleClick);
     // paspaudimas tik viena karta ant to paties langelio:
     cell.addEventListener("click", handleClick, { once: true });
   });
   setBoardHoverClass();
+  winningMessage.classList.remove("show");
 }
 
 function handleClick(e) {
@@ -23,10 +44,33 @@ function handleClick(e) {
   // place the Mark:
   placeMark(cell, curreentClass);
   // check for win
-  // check for draw
-  // switch turns:
-  swapTurns();
-  setBoardHoverClass();
+  if (checkWin(curreentClass)) {
+    endGame(false);
+    // check for draw
+  } else if (isDraw()) {
+    endGame(true);
+    // switch turns:
+  } else {
+    swapTurns();
+    setBoardHoverClass();
+  }
+}
+
+function endGame(draw) {
+  if (draw) {
+    winningMessageText.innerText = "Draw!";
+  } else {
+    winningMessageText.innerText = `${circleTurn ? "O's" : "X's"} Wins!`;
+  }
+  winningMessage.classList.add("show");
+}
+
+function isDraw() {
+  return [...cells].every((cell) => {
+    return (
+      cell.classList.contains(xClass) || cell.classList.contains(circleClass)
+    );
+  });
 }
 
 function placeMark(cell, curreentClass) {
@@ -44,4 +88,12 @@ function setBoardHoverClass() {
   } else {
     board.classList.add(xClass);
   }
+}
+
+function checkWin(curreentClass) {
+  return winningCombination.some((combination) => {
+    return combination.every((i) => {
+      return cells[i].classList.contains(curreentClass);
+    });
+  });
 }
